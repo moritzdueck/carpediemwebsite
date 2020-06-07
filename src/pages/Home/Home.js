4c4fd9bd9fce0d2deff3fecbf3d6b7adc7430f9e
moritzdueck <moritz.dueck@gmail.com>
2020-06-07 11:54:53 +0200

    week selector fixed positioning

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -23,14 +23,6 @@ const Home = () => {
             </div>
 
 
-            <div className='cd_home_block'>
-                <div className='cd_home_text'>
-                    <h2>Hier findest du uns:</h2>
-                    <p>Wir freuen uns schon auf dich.</p>
-                </div>
-                {/* <iframe title='map' id='cd_gmap' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2560.057093192939!2d11.246903415937387!3d50.08521792149452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a1817a404bb0a3%3A0x537f17c28bfa2383!2sCarpe%20Diem!5e0!3m2!1sen!2sde!4v1590644668423!5m2!1sen!2sde" frameborder="0" allowfullscreen="" ariaHidden="false" tabindex="0"></iframe> */}
-                {/* <iframe title='map' id='cd_gmap' width="600" height="450" frameBorder="0"src="https://www.google.com/maps/embed/v1/search?q=Carpe%20Diem%2C%20Pater-Otto-Hopfenm%C3%BCller-Stra%C3%9Fe%2C%20Weismain%2C%20Germany&key=AIzaSyCAdvLqCBFceOeUlfKEX2ufUjuiekbVkAw" allowFullScreen></iframe> */}
-            </div>
          
         </div>
     );
--- src/pages/Kurse/Kurse.js
+++ src/pages/Kurse/Kurse.js
@@ -28,33 +28,33 @@ const Kurse = () => {
 
     return (
         <React.Fragment>
-         {data && <div id='cd_courses_week_selection'>
-            <button onClick={() => setWeekOffset(weekOffset -1)}>&lt;</button>
-            <p>Zeitraum ändern</p>
-            <button onClick={() => setWeekOffset(weekOffset + 1)}>&gt;</button>
-        </div>}
 
-        <div id='cd_courses_wrapper' style={{ gridTemplateColumns: data ? '1fr'.repeat(data.length) : '' }}>
-            {loading && <p>Loading</p>}
-            {error && <p>Error</p>}
+            <div id='cd_courses_wrapper' style={{ gridTemplateColumns: data ? '1fr'.repeat(data.length) : '' }}>
+                {data && <div id='cd_courses_week_selection'>
+                    <button onClick={() => setWeekOffset(weekOffset - 1)}>&lt;</button>
+                    <p>Zeitraum ändern</p>
+                    <button onClick={() => setWeekOffset(weekOffset + 1)}>&gt;</button>
+                </div>}
+                {loading && <p>Loading</p>}
+                {error && <p>Error</p>}
 
-            {!loading && data && [...data].map(day =>
-                <div className='cd_courses_dayCol' key={day[0]}>
-                    <h3>{day[0]}</h3>
-                    <div className='cd_courses_morning'>
-                        {day[1][0].map(item =>
-                            <Kurs id={item.id} item={item} key={item.id} active={item.id === activeID} click={()=>setActiveID(item.id)}/>
-                        )}
+                {!loading && data && [...data].map(day =>
+                    <div className='cd_courses_dayCol' key={day[0]}>
+                        <h3>{day[0]}</h3>
+                        <div className='cd_courses_morning'>
+                            {day[1][0].map(item =>
+                                <Kurs id={item.id} item={item} key={item.id} active={item.id === activeID} click={() => setActiveID(item.id)} />
+                            )}
+                        </div>
+                        <div className='cd_courses_divider' />
+                        <div className='cd_courses_evening'>
+                            {day[1][1].map(item =>
+                                <Kurs id={item.id} item={item} key={item.id} active={item.id === activeID} click={() => setActiveID(item.id)} />
+                            )}
+                        </div>
                     </div>
-                    <div className='cd_courses_divider'/>
-                    <div className='cd_courses_evening'>
-                        {day[1][1].map(item =>
-                            <Kurs id={item.id} item={item} key={item.id} active={item.id === activeID} click={()=>setActiveID(item.id)}/>
-                        )}
-                    </div>
-                </div>
-            )}
-        </div>
+                )}
+            </div>
         </React.Fragment>
     )
 }
