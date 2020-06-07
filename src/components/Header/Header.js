0d030a11efb16b4f70957f6ea4b6b860e2e65747
moritzdueck <moritz.dueck@gmail.com>
2020-06-07 22:40:32 +0200

    changed header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useRef } from 'react'
 import './Header.css'
 import logo from '../../assets/logo.svg'
-import { useLocation, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 
 const Header = () => {
@@ -12,7 +12,7 @@ const Header = () => {
                 collapseMenu()
             }
         },
-        [ref],
+        [],
     )
     useEffect(() => {
         document.addEventListener('click', clickListener)
@@ -21,17 +21,14 @@ const Header = () => {
         }
     }, [clickListener])
 
-    let location = useLocation();
-    console.log(location.pathname);
-    
     return (
         <React.Fragment>
             <header id='cd_header'>
-                <a href='/' id='cd_header_logo'><img src={logo} alt='logo'/></a>
+                <a href='/' id='cd_header_logo'><img src={logo} alt='logo' /></a>
             </header>
-            <nav ref={ref}>
+            <nav>
                 <span id='cd_header_l' />
-                <button id='cd_header_toggle' onClick={toggleMenu}>&#9776; Menu</button>
+                <button id='cd_header_toggle' onClick={toggleMenu} ref={ref}>&#9776; Menu</button>
                 <div id='cd_header_links' className='hidden'>
                     <NavLink to='/' exact activeClassName="cd_selected_link">Home</NavLink>
                     <NavLink to='/kurse' exact activeClassName="cd_selected_link">Kursplan</NavLink>
@@ -39,11 +36,6 @@ const Header = () => {
                     <NavLink to='/gesundheit' exact activeClassName="cd_selected_link">Gesundheit</NavLink>
                     <NavLink to='/rehasport' exact activeClassName="cd_selected_link">Reha-Sport</NavLink>
                     <NavLink to='/wellness' exact activeClassName="cd_selected_link">Wellness</NavLink>
-                    {/* <a href='/kurse'>Kursplan</a>
-                    <a href='/training'>Training</a>
-                    <a href='/gesundheit'>Gesundheit</a>
-                    <a href='/rehasport'>Reha-Sport</a>
-                    <a href='/wellness'>Wellness</a> */}
                 </div>
                 <div id='cd_header_r'>
                     <div id='cd_header_color0' />
