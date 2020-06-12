b34af33b1614403bcf50c44dfdabd391d65011ff
moritzdueck <moritz.dueck@gmail.com>
2020-06-12 11:49:56 +0200

    patch history

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Router, Route } from 'react-router-dom'
 import { createBrowserHistory } from 'history';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
@@ -20,7 +20,7 @@ window.ga && history.listen((location) => {
 });
 
 const App = () => (
-    <Router>
+    <Router history={history}>
       <Header/>
       <Route exact path="/" component={Home} />
       <Route exact path="/kurse" component={Kurse} />
