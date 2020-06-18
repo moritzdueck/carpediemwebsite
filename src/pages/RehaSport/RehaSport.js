97bbe6875bf923ca4c7eecb6758b8f2707b6d0b0
moritzdueck <moritz.dueck@gmail.com>
2020-06-18 14:45:04 +0200

    rehasport texte

--- src/pages/RehaSport/RehaSport.js
+++ src/pages/RehaSport/RehaSport.js
@@ -8,7 +8,7 @@ const RehaSport = () => {
         <div className='content'>
             <div className='cd_home_block'>
                 <div className='cd_home_text'>
-                    <h2>Reha-Sport – Von der Krankenkasse bezuschusst.</h2>
+                    <h2>Rehasport – Von der Krankenkasse übernommen.</h2>
                     <p>Rehabilitationssport hat das Ziel, Ausdauer, Kraft, Koordination und Flexibilität zu verbessern, sowie das Selbstbewusstsein zu stärken und Hilfe zur Selbsthilfe zu bieten. Hilfe zur Selbsthilfe heißt, die Eigenverantwortlichkeit für die Gesundheit zu stärken und die Patienten zum langfristigen, selbstständigen und eigenverantwortlichen Bewegungstraining zu motivieren. Gerade nach einer postoperativen Reha oder nach krankengymnastischen/ physiotherapeutischen Behandlungen stabilisiert der Rehasport durch das weiterführende Training den Behandlungserfolg.</p>
                 </div>
                 <img alt='happy person' src={philipp}></img>
@@ -17,7 +17,35 @@ const RehaSport = () => {
             <div className='cd_home_block'>
                 <div className='cd_home_text'>
                     <h2>Wie funktioniert die Abrechnung mit der Krankenkasse?</h2>
-                    <p>Hier brauchen wir ein paar Informationen und am besten eine Auflistung der nötigen Schritte.</p>
+                    <p>Um von der Krankenkasse übernommen zu werden, wird eine Verordnung von deinem Hausarzt benötigt. 
+                        Eine solche Verordnung darf von jedem niedergelassenen Arzt ausgestellt werden und ist nach §44 
+                        des Sozialgesetzbuches (<a href='https://www.sozialgesetzbuch-sgb.de/sgbix/64.html'>§ 64 SGB IX</a>) 
+                        eine Ergänzende Leistung, welche dir zusteht und nicht verwehrt werden kann. Der Reha Antrag (KV 56) 
+                        liegt oft in der Praxis deines Arztes vor, sodass du diesen nicht im Vorfeld besorgen musst. 
+                        Frag also einfach bei deinem nächsten Hausarztbesuch nach einer Reha-Sport Verordnung!</p>
+                </div>
+            </div>
+
+            <div className='cd_home_block'>
+                <div className='cd_home_text'>
+                    <h2>Wie viele Einheiten umfasst eine genehmigte Verordnung?</h2>
+                    <p>In der Regel umfasst die Verordnung 50 Übungseinheiten, die Gültigkeit ist auf 18 Monate beschränkt. 
+                        Das heißt, du kannst über ein Jahr lang einmal die Woche kostenlos zum Reha-Sport zu uns ins Studio kommen.</p>
+                </div>
+            </div>
+
+            <div className='cd_home_block'>
+                <div className='cd_home_text'>
+                    <h2>Kann ich vor oder nach dem Kurs bei euch trainieren?</h2>
+                    <p>Ja, wir haben mit den aktiven Reha-Sportlern bei uns im Studio ein kleines Zusatzpaket erarbeitet, um die Besuche bei uns zu bereichern. 
+                        Für 7.50€ pro Monat kannst du vor und nach dem Kurs auch auf der Trainingsfläche trainieren und wir erweitern den Kurs für dich von 45 auf 60 min.</p>
+                </div>
+            </div>
+
+            <div className='cd_home_block'>
+                <div className='cd_home_text'>
+                    <h2>Wie oft bietet ihr Rehasport an?</h2>
+                    <p>Die genauen Zeiten findest du auf unserem <a href='/kurse'>Kursplan</a>.</p>
                 </div>
             </div>
         </div>
